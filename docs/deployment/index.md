accf51f5fc546e6dc2af6a8f2dcf43502f36810a
Rudy Marchandise <r.marchandise@sfr.fr>
2021-04-11 19:04:14 +0200

    next

--- docs/deployment/index.md
+++
@@ -1,4 +0,0 @@
-# Deployment
-
-- [Docker](./docker) : Run LarbinBot with your custom Docker Image (ex: to deploy on Heroky, Azure, AWS etc...)
-- [Docker-Compose](./docker-compose) : Run LarbinBot on Docker-Compose (ex: Local development, raspberry, VPS etc...)
---
+++ src/lib/Yaml/YamlFile.ts
@@ -0,0 +1,50 @@
+import { EventType } from '../Events';
+
+export class YamlFile {
+  constructor(
+    public tools: any,
+    public commands = new Array<YamlCommand>(),
+    public schedulers = new Array<YamlScheduler>(),
+    public events = new Array<YamlEvent>(),
+  ){
+  }
+}
+
+export class YamlPolicies {
+  constructor(
+    public admin = false,
+    public mod = false,
+    public vip = false,
+    public sub = false,
+    public others = false
+  ) {
+  }
+}
+
+export class YamlCommand {
+  constructor(
+    public name: string,
+    public random: boolean,
+    public policies: YamlPolicies,
+    public messages = new Array<string>()
+  ){
+  }
+}
+
+export class YamlScheduler {
+  constructor(
+    public id: string,
+    public minutes: number,
+    public random: boolean,
+    public messages = new Array<string>()
+  ) {
+  }
+}
+
+export class YamlEvent {
+  constructor(
+    public name: EventType,
+    public messages = new Array<string>()
+  ) {
+  }
+}
