a1161f860179a15773f6ec4182e6184b8c06d669
Rudy Marchandise <r.marchandise@sfr.fr>
2021-04-11 19:20:38 +0200

    fix

--- www/vue.config.js
+++ www/vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/LarbinBot/'
+    : '/',
   lintOnSave: false,
   transpileDependencies: [
     'vuex-module-decorators',
